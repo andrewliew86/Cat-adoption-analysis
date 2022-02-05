0126673990bec0f9750e13b6e54273366252b2db
andrewliew86 <andrewliew86@yahoo.com.au>
2022-02-05 19:51:06 +1100

    added random forest, xgboost model and more EDA

--- exploratory_data_analysis_15Jan21.py
+++ exploratory_data_analysis_15Jan21.py
@@ -51,4 +51,113 @@ plt.title('Correlation bettween age and fee', fontsize=18)
 plt.xlabel('Fee (dollars)', fontsize=16)
 plt.ylabel('Age (in months)', fontsize=16)
 print("Correlation score between age and fee:")
-print(df['age_in_months'].corr(df['fee']))  #slight negative correlation but  not exacly great -0.34
+print(df['age_in_months'].corr(df['fee']))  
+# Slight negative correlation so older cats tend to be cheaper (but not exacly all that strong -0.34)
+
+
+# Lets look at sex, breed, rsc_grp and location to see if there are any effects on adoption fees
+# There are too many categories for breed, rsc_grp and location so I am going to only look at the top 10 and then the rest I am going to put into 'other'
+# This is mainly for visusalization purposes but might also help with machine learning later
+need_breed = df['breed'].value_counts().index[:11]
+df['breed_rdc_category'] = np.where(df['breed'].isin(need_breed), df['breed'], 'OTHER')
+
+need_rsc_grp = df['rescue_grp'].value_counts().index[:11]
+df['rsc_grp_rdc_category'] = np.where(df['rescue_grp'].isin(need_rsc_grp), df['rescue_grp'], 'OTHER')
+
+need_loc = df['location'].value_counts().index[:11]
+df['location_rdc_category'] = np.where(df['location'].isin(need_loc), df['location'], 'OTHER')
+
+sns.catplot(x="fee", y="breed_rdc_category", data=df, kind='box', orient="h")
+# Some of the breeds e.g. Sphynx, Egyptian Mau cat and ragdoll are very expensive but most are around 200
+
+sns.catplot(x="fee", y="rsc_grp_rdc_category", data=df, kind='box', orient="h")
+# Strong hears farm sanctuary , Give Kitty a Home, Heart & Soul and Maneki Neko Cat Rescue are generally pricier than other rescue groups
+
+sns.catplot(x="fee", y="location_rdc_category", data=df, kind='box', orient="h")
+# Craigiburn and Pakernham seem to be standout places for adoptions
+
+#%% 
+# Lets try using random_forest regressor to see if we can estimate fees
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
+# First, lets remove any rows with NaNs in fees
+df.dropna(subset=['fee'], inplace=True)
+
+# Make the training and test datasets
+X = df[['sex', 'breed_rdc_category', 'location_rdc_category', 'rsc_grp_rdc_category','age_in_months']]
+y = df['fee'].values
+
+# Now, lets deal with the categorical data
+# Get a list of columns that are 'objects' which would then be transformed through pandas dummy encoding
+features_to_encode = X.columns[X.dtypes==object].tolist()
+
+# Convert the features via get_dummies
+def encode_and_bind(original_dataframe, features_to_encode):
+    dummies = pd.get_dummies(original_dataframe[features_to_encode])
+    res = pd.concat([dummies, original_dataframe], axis=1)
+    res = res.drop(features_to_encode, axis=1)
+    return(res)
+
+# Get a new transformed dataframe called X_encoded
+X_encoded = encode_and_bind(X, features_to_encode)
+
+
+# Instantiate a random forest regressor
+rf = RandomForestRegressor(n_estimators=25,
+            random_state=2)
+
+# Create your train and test sets 
+X_train, X_test, y_train, y_test =  train_test_split(X_encoded, y, test_size=0.2)
+
+# Fit rf to your training data
+rf.fit(X_train, y_train)
+            
+
+# Evaluate the test set mean squared error using 3-fold cross validation
+test_scores = cross_val_score(rf, X_test, y_test, scoring='neg_root_mean_squared_error', cv=3)
+
+# Print scores
+print("Using 3-fold cross-val, RMSE of %0.2f with a standard deviation of %0.2f " % (test_scores.mean()*-1, test_scores.std()))
+# We get an RMSE of 99.14 with a standard deviation of 23.40 using 3-fold cross validation
+
+# One of the great things about random forest is that it can tell you which features are most important
+feature_importances = list(zip(X_train, rf.feature_importances_))
+# Sort the feature importances by most important first
+feature_importances_ranked = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+# Plot the top 25 feature importance
+feature_names_25 = [i[0] for i in feature_importances_ranked[:25]]
+y_ticks = np.arange(0, len(feature_names_25))
+x_axis = [i[1] for i in feature_importances_ranked[:25]]
+plt.figure(figsize = (10, 14))
+plt.barh(feature_names_25, x_axis)   #horizontal barplot
+plt.title('Random Forest Feature Importance (Top 25)',
+          fontdict= {'fontname':'Comic Sans MS','fontsize' : 20})
+plt.xlabel('Features',fontdict= {'fontsize' : 16})
+plt.show()
+# As suspected, age is the biggest predictor for fees
+
+#%%
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+
+# First, we deal with the categorical data. So I am using label encoder for this one (instead of one hot encoding)
+encode_df = X.apply(LabelEncoder().fit_transform)
+
+# You need to convert data into a specialized/optimized data structure called DMatrix  
+data_dmatrix = xgb.DMatrix(data=encode_df,label=y, enable_categorical=True)
+
+
+params = {"objective":"reg:squarederror",'colsample_bytree': 0.3,'learning_rate': 0.1,
+                'max_depth': 5, 'alpha': 10}
+
+cv_results = xgb.cv(dtrain=data_dmatrix, params=params, nfold=3,
+                    num_boost_round=70,early_stopping_rounds=10,metrics="rmse", as_pandas=True, seed=123)
+
+# The best I can get is ~104 for the test set so I think this is probably the best we can get without hyperparameter tuning
+
+# Lets visualize boosting trees and feature importances  
+#xg_reg = xgb.train(params=params, dtrain=data_dmatrix, num_boost_round=70)
+#xgb.plot_tree(xg_reg,num_trees=0)
+#plt.show()
---
+++ text_analysis_EDA.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+
+# Here, we are going to use scattertext to uncover any interesting trends from the text
+# See : # https://github.com/JasonKessler/scattertext
+
+import scattertext as st
+import pandas as pd
+import re
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.stem import WordNetLemmatizer 
+from sklearn.feature_extraction.text import TfidfTransformer
+from scipy.sparse.linalg import svds
+
+
+# Read in the clean dataset
+df = pd.read_csv("cat_adoption_clean_dataset_15Jan21.csv", encoding='cp1252')  
+
+# I would like to do some preprocessing first, remove stopwords, lemmatize etc...
+
+def preprocess_words(text): 
+    """
+    Removes URLs in text, makes everything lowercase, substitutes apostrophe, removes dodgy characters and lemmatizes token
+
+    Parameters
+    ----------
+    text : string
+        Text for preprocessing
+
+    Returns
+    -------
+    string
+        Processed string for scattertext visualization
+
+    """
+    # Initiate the Wordnet Lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # Initiate tokenizer
+    tokenizer = RegexpTokenizer(r'\w+')
+
+    # create English stop words list
+    en_stop = get_stop_words('en')
+
+    # Add a few new stop words
+    en_stop.extend(['cat', 'pet', 'foster'])
+    
+    # Remove all urls
+    i = re.sub(r"(www|http:|https:)+\S+", "", text)
+    
+    # Make everything lowercase
+    raw = i.lower()
+    
+    # Replace apostrophe and '-' with underscores 
+    raw = re.sub("['-]", '_', raw)
+    
+    # Tokenize document string
+    tokens = tokenizer.tokenize(raw)
+    
+    # Remove any tokens that are less than 1 character in length
+    # This removes things like 'm' or 'd' or 's' toekns which dont have any meaning
+    tokens_2_chr = [i for i in tokens if len(i) > 1]
+
+    # Lemmatize tokens (so all words are standardized)
+    lemma_tokens = [lemmatizer.lemmatize(i) for i in tokens_2_chr]
+    
+    # Remove stop words from tokens
+    stopped_tokens = [i for i in lemma_tokens if not i in en_stop]
+    
+    # Concatanate the processed text into a string 
+    return " ".join(stopped_tokens)
+
+# Apply the preprocessing function to each row of the 'personality' column 
+df['procesed_text']= df['personality'].apply(preprocess_words)
+
+#%%
+# First, lets look at a scatterplot of the terms associated with male and female cats
+df = df.assign(parse=lambda df: df.procesed_text.apply(st.whitespace_nlp_with_sentences))
+
+corpus = st.CorpusFromParsedDocuments(df, category_col='sex', parsed_col='parse').build().get_unigram_corpus().compact(st.AssociationCompactor(2000))
+
+html = st.produce_scattertext_explorer(
+    corpus,
+    category='Male', category_name='Male', not_category_name='Female',
+    minimum_term_frequency=5, pmi_threshold_coefficient=2,
+    width_in_pixels=1000, metadata=corpus.get_df()['names'],
+    transform=st.Scalers.dense_rank
+)
+open('./scatter_text_cat_sex.html', 'wb').write(html.encode('utf-8'))
+# Although not very strong, 'quiet' 'sweet' 'trust' appears to be terms associated with females
+# For males, it's things like 'big', 'handsome', 'outdoor'
+
+#%%
+# Document-Based Scatterplots
+# Here we use SVD (a dimensionality reduction technique) to group similar cat profiles based on their singula values
+corpus = corpus.add_doc_names_as_metadata(corpus.get_df()['names'])
+
+# Get tf-idf scores of corpus words
+embeddings = TfidfTransformer().fit_transform(corpus.get_term_doc_mat())
+
+# Apply sparse svd and add the first two singular values to the dataframe
+u, s, vt = svds(embeddings, k=3, maxiter=20000, which='LM')
+projection = pd.DataFrame({'term': corpus.get_metadata(), 'x': u.T[0], 'y': u.T[1]}).set_index('term')
+
+# Set the scattertext to show a plot of the grouped cat names (based on their singular values)  
+category = 'Male'
+scores = (corpus.get_category_ids() == corpus.get_categories().index(category)).astype(int)
+html = st.produce_pca_explorer(corpus,
+                               category=category,
+                               category_name='Male',
+                               not_category_name='Female',
+                               metadata=df['names'],
+                               width_in_pixels=1000,
+                               show_axes=False,
+                               use_non_text_features=True,
+                               use_full_doc=True,
+                               projection=projection,
+                               scores=scores,
+                               show_top_terms=False)
+
+open('./scatter_text_cat_sex_svd_grouping.html', 'wb').write(html.encode('utf-8'))
+# We do some groupings (mainly due to the length of the text and descriptions from each company)
+
+#%%
+# One last idea I had was to try and analyze only the adjectives in the description of the cats (beautiful, handsome, cute etc...) and group them to reduce possible 'noise'
+
+# First extract all adjectives
+import spacy
+nlp = spacy.load('en_core_web_sm')
+
+def adj_extractor(doc):
+    """ Extract all the adjectives from text"""
+    doc = nlp(doc)
+    adjectives = []
+    for token in doc:
+        if token.pos_ == 'ADJ':
+            adjectives.append(token)
+    return ' '.join([str(w) for w in adjectives])
+
+# Apply the adj_extractor function to create a new column called 'adjectives'
+df['adjectives'] = df['personality'].apply(adj_extractor)
+
+# Perform SVD with adjectives
+df = df.assign(parse=lambda df: df.adjectives.apply(st.whitespace_nlp_with_sentences))
+corpus = st.CorpusFromParsedDocuments(df, category_col='sex', parsed_col='parse').build().get_unigram_corpus().compact(st.AssociationCompactor(2000))
+corpus = corpus.add_doc_names_as_metadata(corpus.get_df()['names'])
+embeddings = TfidfTransformer().fit_transform(corpus.get_term_doc_mat())
+u, s, vt = svds(embeddings, k=5, maxiter=20000, which='LM')
+projection = pd.DataFrame({'term': corpus.get_metadata(), 'x': u.T[0], 'y': u.T[1]}).set_index('term')
+
+
+category = 'Male'
+scores = (corpus.get_category_ids() == corpus.get_categories().index(category)).astype(int)
+html = st.produce_pca_explorer(corpus,
+                               category=category,
+                               category_name='Male',
+                               not_category_name='Female',
+                               metadata=df['names'],
+                               width_in_pixels=1000,
+                               show_axes=False,
+                               use_non_text_features=True,
+                               use_full_doc=True,
+                               projection=projection,
+                               scores=scores,
+                               show_top_terms=False)
+
+open('./scatter_text_cat_sex_svd_grouping_adjectives.html', 'wb').write(html.encode('utf-8'))
+
+
